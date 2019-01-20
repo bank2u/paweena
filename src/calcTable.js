f6d1cff614d7f7e93bc6d607aeaff57b5366082a
Siwat Jirawiwatpat <siwat.j@b-connex.net>
2019-01-20 17:52:52 +0700

    1st version

--- src/calcTable.js
+++ src/calcTable.js
@@ -1,11 +1,35 @@
 $(function() {
-    $("#txtRawScore, #txtAdjScore").keyup(function() {
-        var rawScore = $("#txtRawScore").val();
-        var adjScore = $("#txtAdjScore").val();
+  $("#error").hide();
 
-        if(rawScore != "" && adjScore != ""){
-            console.log("raw score : " + rawScore);
-            console.log("adjust score : " + adjScore);
+  $("#txtRawScore, #txtAdjScore").keyup(function() {
+    var rawScoreStr = $("#txtRawScore").val();
+    var adjScoreStr = $("#txtAdjScore").val();
+
+    if (rawScoreStr != "" && adjScoreStr != "") {
+      console.log("raw score : " + rawScoreStr);
+      console.log("adjust score : " + adjScoreStr);
+      rawScore = parseInt(rawScoreStr);
+      adjScore = parseInt(adjScoreStr);
+
+      if (rawScore < adjScore) {
+        $("#error").show();
+        $("#tblScore").hide();
+      } else {
+        $("#tblScore>tbody").empty();
+
+        for (var i = 1; i <= rawScore; i++) {
+          var rowNo = i;
+          var per = Math.round((((i * 100) / rawScore) * 100)) / 100;
+          var adj = Math.round((i * adjScore) / rawScore);
+          var row =
+            "<tr><td>" + i + "</td><td>" + per + "</td><td>" + adj + "</td>";
+
+          $("#tblScore>tbody").append(row);
         }
-    })
+
+        $("#tblScore").show();
+        $("#error").hide();
+      }
+    }
+  });
 });
